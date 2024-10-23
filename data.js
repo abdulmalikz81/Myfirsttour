b1616977f14558d5f179dbbd813e19f411faef73
abdulmalikz81 <abdulmalikz81@gmail.com>
2024-10-23 15:43:45 +0800

    First Commit

---
+++ data.js
@@ -0,0 +1,158 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0.7853981633974483,
+          "target": "0-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0477244495989488,
+          "pitch": -0.10421456006480234,
+          "title": "New Cafe Hebrew",
+          "text": "The home of coffee"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.17505874679860334,
+        "pitch": 0,
+        "fov": 1.2896551808019092
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0213113233958655,
+          "pitch": 0.1963231271164716,
+          "rotation": 0,
+          "target": "2-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.17505874679860334,
+          "pitch": 0,
+          "title": "Engineering Auditorium",
+          "text": "Home for lectures adnd tutorials"
+        },
+        {
+          "yaw": 2.632298614737987,
+          "pitch": -0.32892720520452556,
+          "title": "Kentridge Residences",
+          "text": "Residences for lecturers"
+        }
+      ]
+    },
+    {
+      "id": "2-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2896551808019092
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.35454174698794105,
+          "pitch": -0.03337444978541626,
+          "rotation": 2.356194490192345,
+          "target": "0-engineering-auditorium-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6919957070478713,
+          "pitch": 0.02046959329393694,
+          "title": "Welcome to Engineering",
+          "text": "This is where engineers are being made"
+        },
+        {
+          "yaw": 2.9445786701212384,
+          "pitch": -0.2481956696777612,
+          "title": "UCC",
+          "text": "This is where many functions are being held."
+        }
+      ]
+    }
+  ],
+  "name": "Faculty of Engineering",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
